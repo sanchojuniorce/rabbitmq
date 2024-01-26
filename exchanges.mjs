eb8cf8a7ff83efbed6b41872903841fc1866858a
Sancho Júnior <sanchorodriguesnovooriente@gmail.com>
2024-01-26 10:02:04 -0300

    Topic Exchange

--- exchanges.mjs
+++ exchanges.mjs
@@ -14,19 +14,42 @@ async function exchange() {
   await channel.assertExchange('udemy_exchange', 'direct')
 
   // Criar uma fila 
-  await channel.assertQueue('udemy_notification', {
+  await channel.assertQueue('udemy_push_notification', {
+    durable: true
+  })
+  await channel.assertQueue('udemy_emal_notification', {
     durable: true
   })
 
   // Binding - Linkar fila com exchange
-  await channel.bindQueue('udemy_notification', 'udemy_exchange','novoCurso')
+  await channel.bindQueue(
+    'udemy_push_notification', 
+    'udemy_exchange',
+    'novoCurso'
+  );
+  await channel.bindQueue(
+    'udemy_emal_notification', 
+    'udemy_exchange',
+    'novoCurso'
+  );
+  await channel.bindQueue(
+    'udemy_emal_notification', 
+    'udemy_exchange',
+    'diploma'
+  );
+  //await channel.unbindQueue('udemy_push_notification', 'udemy_exchange','novoCurso')
 
   // Publicando mensagem com chave de roteamento
   channel.publish(
     'udemy_exchange', 
     'udemy_notification', 
+    //'diploma', 
     Buffer.from('Teste mensagem')
   );
+
+  await channel.close()
+  await conn.close()
+
 }
 
 exchange();
