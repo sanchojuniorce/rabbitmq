298f1d666d0131bcbc811eb6d8a34c94a0646dae
Sancho Júnior <sanchorodriguesnovooriente@gmail.com>
2024-02-03 22:01:36 -0300

    Max Priority

---
+++ queues/max-priority.mjs
@@ -0,0 +1,29 @@
+import amqp from 'amqplib';
+
+async function main() {
+  const connection = await amqp.connect({
+    hostname: 'localhost',
+    port: 5672,
+    username: 'rabbitmq',
+    password: 'curso'
+  })
+
+  // Criando canal de comunicação
+  const channel = await connection.createChannel()
+
+  await channel.assertQueue('priority', {
+    maxPriority: 5
+  })
+  
+  // for(let i=0; i<10; i++){
+  //   channel.publish('', 'priority', Buffer.from('Mensagem padrão sem prioridade'))
+  // }
+  channel.publish('', 'priority', Buffer.from('Mensagem com prioridade 1'), {
+    priority: 1
+  })
+
+  await channel.close()
+  await connection.close()
+}
+
+main();
