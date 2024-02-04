d32c7b3ccf643eb5d219a15d17c41ef7155c00cb
Sancho Júnior <sanchorodriguesnovooriente@gmail.com>
2024-02-04 11:20:49 -0300

    AutoDelete

---
+++ queues/autoDelete.mjs
@@ -0,0 +1,29 @@
+import amqp from "amqplib";
+
+async function exclusive() {
+  const connection = await amqp.connect({
+    hostname: "localhost",
+    port: 5672,
+    username: "rabbitmq",
+    password: "curso",
+  });
+
+  // Criando canal de comunicação
+  const channel = await connection.createChannel();
+
+  await channel.assertQueue('auto_delete', {
+    autoDelete: true
+  })
+
+  // for(let i=0; i<100; i++){
+  //   channel.publish('', 'auto_delete', Buffer.from(`Minha fila auto-delete`));
+  // }  
+
+  await channel.prefetch(10)
+  channel.consume('auto_delete', (data) => {
+    console.log(data.content.toString())
+  })
+
+}
+
+exclusive();
